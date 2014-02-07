1132410f91b93800c9e5e3bf265c75881e0b967a
Josh Larson <jplhomer@gmail.com>
2014-02-07 11:16:41 -0600

    Implement nodemailer and send task for test emails

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 
 	var path = require('path');
 
+    // Allow user to specify template to send through the CLI
+    var template = grunt.option('template');
+
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
@@ -74,6 +77,8 @@ module.exports = function(grunt) {
 
         /**
          * Insert the media query css into the document manually.
+         * 
+         * Remember: you'll need to update each style section as you add/remove templates.
          */
         htmlbuild: {
             dist: {
@@ -82,7 +87,10 @@ module.exports = function(grunt) {
                 options: {
                     relative: true,
                     styles: {
-                        mq: '<%= paths.css %>/mq.css'
+                        basic: ['<%= paths.css%>/basic.css','<%= paths.css %>/mq.css'],
+                        hero: ['<%= paths.css%>/hero.css','<%= paths.css %>/mq.css'],
+                        sidebar: ['<%= paths.css%>/sidebar.css','<%= paths.css %>/mq.css'],
+                        sidebarHero: ['<%= paths.css%>/sidebar-hero.css','<%= paths.css %>/mq.css']
                     }
                 }
             }
@@ -102,6 +110,39 @@ module.exports = function(grunt) {
                     src: ['**/*.{gif,png,jpg}'],
                     dest: '<%= paths.dist %>/img'
                 }]
+            }
+        },
+
+        // Send a test email to whomever we choose
+        nodemailer: {
+
+            options: {
+
+                /**
+                 * Defaults to sendmail if you don't specify a transport below
+                 * See config/nodemailer-transport.json for a sample Gmail config,
+                 * or visit https://github.com/andris9/Nodemailer for all options
+                 */
+                transport: grunt.file.readJSON('config/nodemailer-transport.json'),
+
+                // By default, generate text from our HTML template automatically
+                generateTextFromHTML: true,
+
+                message: {
+                    from: 'From Name <address@email.com>',
+                    subject: 'Email template test'
+                },
+
+                // A collection of recipients
+                recipients: [{
+                    email: 'your@recipient.com',
+                    name: 'Recipient Name'
+                }]
+            },
+
+            // Load the HTML template from our dist directory.
+            dist: {
+                src: ['<%= paths.dist %>/' + template + '.html']
             }
         },
 
@@ -179,6 +220,18 @@ module.exports = function(grunt) {
 
     // To distribute, do the other two steps
     grunt.registerTask('dist', ['compass', 'clean', 'imagemin', 'render:dist', 'premailer', 'htmlbuild']);
+
+    // Register a send tast to simulate an email delivery
+    grunt.registerTask('send', 'Simulates an email delivery.', function() {
+        if ( !template ) {
+            grunt.fail.fatal('You need to specify a template to send: --template=templateName');
+        }
+
+        grunt.task.run([
+            'dist',
+            'nodemailer'
+        ]);
+    });
 
     // By default, do the dev version
 	grunt.registerTask('default', ['dev']);
